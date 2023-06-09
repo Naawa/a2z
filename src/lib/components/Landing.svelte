b5193b6f0a1988748e1b249eb36ab2419b4ddb18
Nawa <n.a.w.a@outlook.com>
2023-06-09 16:29:41 -0700

    Touched Landing.svelte, global.scss, etc...

---
+++ src/lib/components/Landing.svelte
@@ -0,0 +1,5 @@
+<h1>A2Z</h1>
+<p>A2Z aims to provide a platform where people can use their skills to help others on their own time. If you are a tradesman looking for some extra income, or, if you are a person that is looking for affordable and fast help with any kind of plumbing, electrical, or renovation work, A2Z is the place for you.</p>
+<p>
+   <i>Anything you need, from A2Z.</i>
+</p>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import "$lib/global/global.scss"
+</script>
+
+<slot />
