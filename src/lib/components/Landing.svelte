3c3bfe3544365d5b143af7eae447a6267143d7f8
Nawa <n.a.w.a@outlook.com>
2023-06-09 17:09:53 -0700

    Fix responsive layout.

--- src/lib/components/Landing.svelte
+++ src/lib/components/Landing.svelte
@@ -1,12 +1,22 @@
-<h1>A2Z</h1>
-<p>
-   A2Z aims to provide a platform where people can use their skills to help others on their own time. If you are a tradesman looking for some extra income, or, if you are a person that is looking for affordable and fast help with any kind of plumbing, electrical, or renovation work, A2Z is the place for you.
-</p>
-<p>
-   <i>Anything you need, from A2Z.</i>
-   <br>
-   <br>
-   <i>Soheil, Founder & CEO</i>
-   <br>
-   <i>Nawaz, CTO</i>
-</p>
+<style lang="scss">
+   div {
+      margin: 10vh auto;
+      max-width: 800px;
+   }
+</style>
+
+<div>
+   <h1>A2Z</h1>
+   <p>
+      A2Z aims to provide a platform where people can use their skills to help others on their own time. If you are a tradesman looking for some extra income, or, if you are a person that is looking for affordable and fast help with any kind of plumbing, electrical, or renovation work, A2Z is the place for you.
+   </p>
+   <p>
+      <br>
+      <i>Anything you need, from A2Z.</i>
+      <br>
+      <br>
+      <i>Soheil, Founder & CEO</i>
+      <br>
+      <i>Nawaz, CTO</i>
+   </p>
+</div>
