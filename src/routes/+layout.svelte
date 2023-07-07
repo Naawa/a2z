4e97945b8e3ce03f9ae82292f64dfdeeb0ce56bd
Nawa <n.a.w.a@outlook.com>
2023-07-06 17:34:12 -0700

    Settled for css load transition.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,22 +3,14 @@
     import Footer from "$lib/layout/Footer.svelte";
     import "$lib/scss/global.scss";
     import { fly } from "svelte/transition";
-    import { onMount } from 'svelte';
-
-    let mounted = false;
-
-    onMount(() => mounted = true);
-
     export let data;
 </script>
 <Navigation></Navigation>
-{#if mounted}
-    <main>
-        {#key data.url}
-            <div in:fly={{ y: 200, duration: 300,delay: 300}} out:fly={{ y: 200, duration: 300 }}>
-                <slot />
-            </div>
-        {/key}
-    </main>
-{/if}
+<main>
+    {#key data.url}
+        <div in:fly={{ y: 200, duration: 300,delay: 300}} out:fly={{ y: 200, duration: 300 }}>
+            <slot />
+        </div>
+    {/key}
+</main>
 <Footer></Footer>
