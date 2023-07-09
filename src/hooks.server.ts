46578633c7e11099368a7da0d5a764329a0a487d
Nawa <n.a.w.a@outlook.com>
2023-07-09 01:25:42 -0700

    Fixing redirect in hooks.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,5 @@
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
 import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit'
-import { redirect } from '@sveltejs/kit'
 
 export const handle = async ({ event, resolve }) => {
   event.locals.supabase = createSupabaseServerClient({
