f878ef963311e60f9dc4b313fc57d5e176e7df69
Nawa <n.a.w.a@outlook.com>
2023-07-11 12:48:15 -0700

    Adjust nav.

--- src/lib/layout/Navigation.svelte
+++ src/lib/layout/Navigation.svelte
@@ -33,8 +33,8 @@
     <div id="logo">a2z</div>
     <div>
         <a id="first" href="/" class:active={$page.url.pathname==='/'}>Home</a>
-        {#if $page.url.pathname==='/dashboard'}
-            <a id="first" href="/dashboard" class:active={$page.url.pathname==='/dashboard'}>Welcome</a>
+        {#if $page.data.supabase.getSession()}
+            <a id="first" href="/dashboard" class:active={$page.url.pathname==='/dashboard'}>Dashboard</a>
         {:else}
             <a id="second" href="/login" class:active={$page.url.pathname==='/login' || $page.url.pathname==='/register'}>Get Started</a>
             {/if}
