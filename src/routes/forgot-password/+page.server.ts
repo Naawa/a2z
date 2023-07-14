286795dcf9124a2dba980beacde0182846b1d96d
Nawa <n.a.w.a@outlook.com>
2023-07-13 22:17:07 -0700

    Fixed update password form.

--- src/routes/forgot-password/+page.server.ts
+++ src/routes/forgot-password/+page.server.ts
@@ -1,5 +1,4 @@
 import { emailForLinkSchema } from "$lib/utils/validationSchema.js";
-import { redirect } from "@sveltejs/kit"
 import { message, superValidate } from 'sveltekit-superforms/server';
 
 export const load = async ( event ) => {
